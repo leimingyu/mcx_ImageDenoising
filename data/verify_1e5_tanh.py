76dc1937f1ded69044bff52a6efb1bb2d377d818
Leiming Yu <leimingyu830@gmail.com>
2018-02-12 21:16:31 -0500

    1e5 model using tanh

---
+++ data/verify_1e5_tanh.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import argparse, os
+import numpy as np
+import tensorflow as tf
+import scipy.io as spio
+
+from model import denoiser
+from utils import *
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--checkpoint_dir', dest='ckpt_dir', default='./checkpoint_1e5_tanh', help='models are saved here')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=128, help='# images in batch')
+parser.add_argument('--use_gpu', dest='use_gpu', type=int, default=1, help='gpu flag, 1 for GPU and 0 for CPU')
+
+args = parser.parse_args()
+
+#parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
+#parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
+
+
+
+
+
+
+
+
+def denoiser_test(denoiser):
+
+    #--------------------------------------------------------------------------
+    # plan B : apply log(x+1) to the raw value 
+    #--------------------------------------------------------------------------
+
+    #------------
+    # 1e8 
+    #------------
+    noisy1 = './osa_data/1e+08/1/osa_phn1e+08_test1_img1.mat'
+    noisy50 = './osa_data/1e+08/1/osa_phn1e+08_test1_img50.mat'
+
+    noisymat1  = spio.loadmat(noisy1, squeeze_me=True) # the output is a dict
+    noisymat50 = spio.loadmat(noisy50, squeeze_me=True) # the output is a dict
+
+    noisyData1  = noisymat1['currentImage'] 
+    noisyData50 = noisymat50['currentImage'] 
+
+    (im_h, im_w) = noisyData1.shape
+
+    noisyData1  = np.reshape(noisyData1, (im_h, im_w, 1))  # extend one dimension
+    noisyData50 = np.reshape(noisyData50, (im_h, im_w, 1))  # extend one dimension
+
+    # normalize data
+    noisyData1   = np.tanh(noisyData1  / 4e7)
+    noisyData50  = np.tanh(noisyData50 / 4e7)
+
+
+    input_noisy1  = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+    input_noisy50 = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+
+    # update
+    input_noisy1[0, :, :, :]  = noisyData1
+    input_noisy50[0, :, :, :] = noisyData50
+
+    denoiser.test(input_noisy1,    ckpt_dir=args.ckpt_dir, outFile='1e5model-1e8-tanh_test1_img1.mat')
+    denoiser.test(input_noisy50,   ckpt_dir=args.ckpt_dir, outFile='1e5model-1e8-tanh_test1_img50.mat')
+
+
+    #------------
+    # 1e7 
+    #------------
+    noisy1 = './osa_data/1e+07/1/osa_phn1e+07_test1_img1.mat'
+    noisy50 = './osa_data/1e+07/1/osa_phn1e+07_test1_img50.mat'
+
+    noisymat1  = spio.loadmat(noisy1, squeeze_me=True) # the output is a dict
+    noisymat50 = spio.loadmat(noisy50, squeeze_me=True) # the output is a dict
+
+    noisyData1  = noisymat1['currentImage'] 
+    noisyData50 = noisymat50['currentImage'] 
+
+    (im_h, im_w) = noisyData1.shape
+
+    noisyData1  = np.reshape(noisyData1, (im_h, im_w, 1))  # extend one dimension
+    noisyData50 = np.reshape(noisyData50, (im_h, im_w, 1))  # extend one dimension
+
+    # normalize data
+    noisyData1   = np.tanh(noisyData1  / 4e7)
+    noisyData50  = np.tanh(noisyData50 / 4e7)
+
+
+    input_noisy1  = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+    input_noisy50 = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+
+    # update
+    input_noisy1[0, :, :, :]  = noisyData1
+    input_noisy50[0, :, :, :] = noisyData50
+
+    denoiser.test(input_noisy1,    ckpt_dir=args.ckpt_dir, outFile='1e5model-1e7-tanh_test1_img1.mat')
+    denoiser.test(input_noisy50,   ckpt_dir=args.ckpt_dir, outFile='1e5model-1e7-tanh_test1_img50.mat')
+
+
+    #------------
+    # 1e6 
+    #------------
+    noisy1 = './osa_data/1e+06/1/osa_phn1e+06_test1_img1.mat'
+    noisy50 = './osa_data/1e+06/1/osa_phn1e+06_test1_img50.mat'
+
+    noisymat1  = spio.loadmat(noisy1, squeeze_me=True) # the output is a dict
+    noisymat50 = spio.loadmat(noisy50, squeeze_me=True) # the output is a dict
+
+    noisyData1  = noisymat1['currentImage'] 
+    noisyData50 = noisymat50['currentImage'] 
+
+    (im_h, im_w) = noisyData1.shape
+
+    noisyData1  = np.reshape(noisyData1, (im_h, im_w, 1))  # extend one dimension
+    noisyData50 = np.reshape(noisyData50, (im_h, im_w, 1))  # extend one dimension
+
+    # normalize data
+    noisyData1   = np.tanh(noisyData1  / 4e7)
+    noisyData50  = np.tanh(noisyData50 / 4e7)
+
+
+
+    input_noisy1  = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+    input_noisy50 = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+
+    # update
+    input_noisy1[0, :, :, :]  = noisyData1
+    input_noisy50[0, :, :, :] = noisyData50
+
+    denoiser.test(input_noisy1,    ckpt_dir=args.ckpt_dir, outFile='1e5model-1e6-tanh_test1_img1.mat')
+    denoiser.test(input_noisy50,   ckpt_dir=args.ckpt_dir, outFile='1e5model-1e6-tanh_test1_img50.mat')
+
+    #------------
+    # 1e5 
+    #------------
+    noisy1 = './osa_data/1e+05/1/osa_phn1e+05_test1_img1.mat'
+    noisy50 = './osa_data/1e+05/1/osa_phn1e+05_test1_img50.mat'
+
+    noisymat1  = spio.loadmat(noisy1, squeeze_me=True) # the output is a dict
+    noisymat50 = spio.loadmat(noisy50, squeeze_me=True) # the output is a dict
+
+    noisyData1  = noisymat1['currentImage'] 
+    noisyData50 = noisymat50['currentImage'] 
+
+    (im_h, im_w) = noisyData1.shape
+
+    noisyData1  = np.reshape(noisyData1, (im_h, im_w, 1))  # extend one dimension
+    noisyData50 = np.reshape(noisyData50, (im_h, im_w, 1))  # extend one dimension
+
+    # normalize data
+    noisyData1   = np.tanh(noisyData1  / 4e7)
+    noisyData50  = np.tanh(noisyData50 / 4e7)
+
+
+
+    input_noisy1  = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+    input_noisy50 = np.zeros((1, im_h, im_w, 1), dtype=np.float32) # 4D matrix
+
+    # update
+    input_noisy1[0, :, :, :]  = noisyData1
+    input_noisy50[0, :, :, :] = noisyData50
+
+    denoiser.test(input_noisy1,    ckpt_dir=args.ckpt_dir, outFile='1e5model-1e5-tanh_test1_img1.mat')
+    denoiser.test(input_noisy50,   ckpt_dir=args.ckpt_dir, outFile='1e5model-1e5-tanh_test1_img50.mat')
+
+def main(_):
+    if args.use_gpu:
+        # added to control the gpu memory
+        print("Run Tensorflow [GPU]\n")
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+            model = denoiser(sess)
+            denoiser_test(model)
+    else:
+        ##print("CPU\n")
+        print "CPU Not supported yet!"
+        sys.exit(1)
+        ##with tf.Session() as sess:
+        ##    model = denoiser(sess, sigma=args.sigma)
+        ##    if args.phase == 'train':
+        ##        denoiser_train(model, lr=lr)
+        ##    elif args.phase == 'test':
+        ##        denoiser_test(model)
+        ##    else:
+        ##        print('[!]Unknown phase')
+        ##        exit(0)
+
+
+if __name__ == '__main__':
+    tf.app.run()
